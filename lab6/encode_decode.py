d86f9665946e8f0d019f6b3e1925cc3ce9c9c36e
aarithi123 <148919216+aarithi123@users.noreply.github.com>
2024-05-05 16:00:42 -0400

    Update encode_decode.py

--- lab6/encode_decode.py
+++ lab6/encode_decode.py
@@ -1,1 +1,20 @@
+new_password = ''
 
+print("Menu")
+print("-" * 12)
+print("1. Encode")
+print("2. Decode")
+print("3. Quit")
+print()
+user_option = int(input("Please enter an option: "))
+
+menu_option = True
+
+while menu_option:
+    password = input("Please enter your password to encode: ")
+    if user_option == 1:
+        for n in range(0, len(password)):
+            x = int(password[n]) + 3
+            new_password += str(x)
+
+    print(new_password)
