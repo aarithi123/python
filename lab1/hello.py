6cfa6bdfd370f6732332ef32c13d27c2bd7d3a38
aarithi123 <148919216+aarithi123@users.noreply.github.com>
2024-05-05 15:52:57 -0400

    Add files via upload

--- lab1/hello.py
+++ lab1/hello.py
@@ -1,1 +1,7 @@
-
+# Ask user for name, age, and location
+name = input("Hello. What is your name? ")
+print("It's nice to meet you, " + name + ".", end = ' ')
+age = input("How old are you? ")
+print("I see that you are still quite young at only " + age + ".")
+location = input("Where do you live? ")
+print("Wow! I've always wanted to go to " + location + ". Thanks for chatting with me. Bye!")
